b5944e7a55f972c431f1d55811a58d435f224a6f
Smit <smitrpatel19@gnu.ac.in>
2022-08-17 03:31:24 +0530

    added

---
+++ diff.py
@@ -0,0 +1,4 @@
+#for simple sum of numbers
+a = 5
+b = 6
+print("Sum Of Entered Two Number : ",a+b)
--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
 #for simple sum of numbers
 a = 5
 b = 6
-print("Sum Of Entered Two Number : ",a+b)
+print("Sum of entered two Number : ",a+b)
